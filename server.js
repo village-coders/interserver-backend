ab56f473d22414203acebcabb46b692c55a6fb98
village-coders <villagecoders7@gmail.com>
2026-03-25 13:58:38 +0100

    sfsdsdffs

--- server.js
+++ server.js
@@ -31,6 +31,10 @@ app.get('*', (req, res) => {
 });
 
 // Start Server
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+if(process.env.NODE_ENV !== "production"){
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}
+
+module.exports = app;
